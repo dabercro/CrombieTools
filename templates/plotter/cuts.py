9c372fc69549d1a5933aaa03531396102b38fde8
dabercro <dabercro@mit.edu>
2016-06-08 12:22:41 +0200

    Some more pythonic way of doing cuts.

--- templates/plotter/cuts.py
+++ templates/plotter/cuts.py
@@ -1,17 +1,23 @@
-# These are just for the users to loop over
-
-categories = ['example']
-regions    = ['signal']
-
 # Two dictionaries to define the cuts for separate categories and control regions
 
 categoryCuts = {
     'example' : '1'
     }
 
+
 regionCuts = {
     'signal' : '1'
     }
+
+# These are just for the users to loop over
+
+categories = categoryCuts.keys()
+regions    = regionCuts.keys()
+
+# Making selection of multiple entries
+
+def joinCuts(toJoin=regionCuts.keys(), cuts=regionCuts):
+    return ' && '.join([cuts[cut] for cut in toJoin])
 
 # A weight applied to all MC
 
@@ -26,14 +32,15 @@ additions    = { # key : [Data,MC]
 
 # Do not change the names of these functions or required parameters
 # Otherwise you cannot use some convenience functions
+# Multiple regions are concatenated with '+'
 # Generally you can probably leave these alone
 
 def cut(category, region):
-    return '((' + categoryCuts[category] + ') && (' + regionCuts[region] + '))'
+    return '((' + categoryCuts[category] + ') && (' + joinCuts(toJoin=region.split('+')) + '))'
 
 def dataMCCuts(region, isData):
     key = 'default'
-    if additions.get(region):
+    if region in additions.keys():
         key = region
 
     index = 1
