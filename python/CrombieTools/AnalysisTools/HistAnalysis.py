b9f7deb7fd7132d7ac3f7429b4ab3e92cfc5362e
dabercro <dabercro@mit.edu>
2016-06-22 10:00:03 +0200

    Moved to plotter. Needs fixing anyway.

--- python/CrombieTools/AnalysisTools/HistAnalysis.py
+++ python/CrombieTools/AnalysisTools/HistAnalysis.py
@@ -12,23 +12,22 @@ def SetupFromEnv(analyzer=histAnalysis):
     import os
 
     ## @todo place the FileConfigReader functions into its own setup function for other classes to call
-    if os.path.exists('CrombieAnalysisConfig.sh'):
-        def readMC(config):
-            analyzer.ReadMCConfig(config,analyzer.kBackground)
-        def readSignal(config):
-            analyzer.ReadMCConfig(config,analyzer.kSignal)
-
-        targets = [[analyzer.SetLuminosity, 'CrombieLuminosity'],
-                   [analyzer.SetInDirectory, 'CrombieInFilesDir'],
-                   [readMC,'CrombieMCConfig'],
-                   [readSignal,'CrombieSignalConfig']
-                   ]
-        for target in targets:
-            if os.environ.get(target[1]) == None:
-                print 'No ' + target[1] + ' in config'
-            else:
-                try:
-                    target[0](os.environ[target[1]])
-                except:
-                    target[0](float(os.environ[target[1]]))
+    def readMC(config):
+        analyzer.ReadMCConfig(config,analyzer.kBackground)
+    def readSignal(config):
+        analyzer.ReadMCConfig(config,analyzer.kSignal)
+            
+    targets = [[analyzer.SetLuminosity, 'CrombieLuminosity'],
+               [analyzer.SetInDirectory, 'CrombieInFilesDir'],
+               [readMC,'CrombieMCConfig'],
+               [readSignal,'CrombieSignalConfig']
+               ]
+    for target in targets:
+        if os.environ.get(target[1]) == None:
+            print 'No ' + target[1] + ' in config'
+        else:
+            try:
+                target[0](os.environ[target[1]])
+            except:
+                target[0](float(os.environ[target[1]]))
 
