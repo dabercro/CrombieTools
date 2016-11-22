20936f6b8caf91ed79d35c9c0a5c44f03aec2891
dabercro <dabercro@mit.edu>
2016-11-22 16:44:26 +0100

    findtree now runs on a bunch of files.

--- scripts/findtree.py
+++ scripts/findtree.py
@@ -24,21 +24,28 @@ def GetNumEntries(fileName, className):
     return NumberOfEvents
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    if len(sys.argv) == 1:
         print('Wrong number of args!')
         exit(1)
 
-    if os.path.isfile(sys.argv[1]):
-        NumberOfEvents = GetNumEntries(sys.argv[1], 'TTree')
-    else:
-        print('Error, file does not exist.')
-        exit(1)
+    exitcode = 0
 
-    if NumberOfEvents < 0:
-        # This means that no keys were found in the file
-        exit(5)
-    elif NumberOfEvents != 0:
-        exit(0)
-    else:
-        print('Did not successfully find tree with events.')
-        exit(1)
+    for checkfile in sys.argv[1:]:
+        print 'Searching', checkfile
+
+        if os.path.isfile(sys.argv[1]):
+            NumberOfEvents = GetNumEntries(sys.argv[1], 'TTree')
+        else:
+            print('Error, file does not exist.')
+            exitcode += 1
+
+        if NumberOfEvents < 0:
+            # This means that no keys were found in the file
+            exitcode += 100
+        elif NumberOfEvents == 0:
+            print('Did not successfully find tree with events.')
+            exitcode += 1
+
+    print 'Exiting with exit code:', exitcode
+
+    exit(exitcode)
