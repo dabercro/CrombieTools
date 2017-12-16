de9f15e2baebd30db10fc83779ef23d0a9c756c2
dabercro <dabercro@mit.edu>
2017-12-16 16:16:16 -0500

    Return absolute values for deltaPhi.

--- PlotTools/interface/KinematicFunctions.h
+++ PlotTools/interface/KinematicFunctions.h
@@ -16,9 +16,9 @@ float deltaPhi(float phi1, float phi2)
 {
   float PHI = fabs(phi1-phi2);
   if (PHI<=3.14159265)
-    return PHI;
+    return fabs(PHI);
   else
-    return 2*3.14159265-PHI;
+    return fabs(2*3.14159265-PHI);
 }
 
 /// Calculate deltaR between two particles
