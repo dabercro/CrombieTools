6f9b0c7cf516cf6017e25f74b53f845d7c064911
dabercro <dabercro@mit.edu>
2017-12-17 14:07:51 -0500

    Add additional information to the store.

--- SkimmingTools/interface/StoreParticles.h
+++ SkimmingTools/interface/StoreParticles.h
@@ -18,17 +18,24 @@ class ObjectStore {
   };
 
  ObjectStore(std::initializer_list<E> sorted_enums,
-             std::function<F(T*)> compare, order which = order::eDesc)
+             std::function<F(T*)> compare,
+             order which = order::eDesc)
    : compare(compare), which_order(which), total(sorted_enums.size()) {
     for (auto valid_enum : sorted_enums)
-      store.push_back({valid_enum, nullptr});
+      store.push_back({valid_enum, nullptr, 0});
   }
 
   ~ObjectStore () {}
 
-  std::vector<std::pair<E, T*>> store;
+  struct Particle {
+    const E branch;
+    T* particle;
+    int flag;
+  };
 
-  void check (T& obj) {
+  std::vector<Particle> store;
+
+  void check (T& obj, int flag = 0) {
     T* addr = &obj;
     // Get the first empty iterator or the end
     auto it = store.begin() + num_filled;
@@ -36,14 +43,21 @@ class ObjectStore {
       num_filled++;
     // Insertion sort: find location
     for (;it != store.begin(); --it) {
-      if ((compare(addr) < compare((it - 1)->second)) == (which_order == order::eDesc))
+      if ((compare(addr) < compare((it - 1)->particle)) == (which_order == order::eDesc))
         break;
     }
     // Insertion sort: move elements down
-    for(T* temp = nullptr; it != store.end() && addr; ++it) {
-      temp = it->second;
-      it->second = addr;
-      addr = temp;
+    { // Extra scope so temp_flag doesn't leak
+      int temp_flag = 0;
+      for(T* temp = nullptr; it != store.end() && addr; ++it) {
+        temp = it->particle;
+        it->particle = addr;
+        addr = temp;
+
+        temp_flag = it->flag;
+        it->flag = flag;
+        flag = temp_flag;
+      }
     }
   }
 
