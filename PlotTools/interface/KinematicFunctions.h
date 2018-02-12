1a123df79e2fb87989194d805c9143be5c4d39f7
dabercro <dabercro@mit.edu>
2018-02-12 17:21:44 -0500

    Small changes to types and names.

--- PlotTools/interface/KinematicFunctions.h
+++ PlotTools/interface/KinematicFunctions.h
@@ -12,33 +12,44 @@
 #include "TVector2.h"
 #include "TLorentzVector.h"
 
-/// Calculate deltaPhi between two particles
-float deltaPhi(float phi1, float phi2) {
+namespace {
   const double pi = 3.14159265358979323846;
-  // Make phi between +- pi, if -pi < phi < 3*pi
-  auto make_pm = [pi](float phi) {
-    if (phi > pi)
-      phi -= 2*pi;
-    return phi;
-  };
+}
 
+/// Make phi that are from [0, 2pi) to [-pi, pi)
+double make_pm(double phi) {
+  if (phi >= pi)
+    phi -= 2*pi;
+  return phi;
+}
+
+/// Make phi that are from [-pi, pi) to [0, 2pi)
+double make_2pi(double phi) {
+  if (phi < 0)
+    phi += 2*pi;
+  return phi;
+}
+
+/// Calculate deltaPhi between two particles
+double deltaPhi(double phi1, double phi2) {
+  // Make phi between +- pi, if -pi < phi < 3*pi
   return fabs(make_pm(fabs(make_pm(phi1) - make_pm(phi2))));
 }
 
 /// Calculate deltaR2 between two particles
-float deltaR2(float eta1, float phi1, float eta2, float phi2) {
+double deltaR2(double eta1, double phi1, double eta2, double phi2) {
   auto d_eta = eta2 - eta1;
   auto d_phi = deltaPhi(phi1, phi2);
   return d_eta * d_eta + d_phi * d_phi;
 }
 
 /// Calculate deltaR between two particles
-float deltaR(float eta1, float phi1, float eta2, float phi2) {
+double deltaR(double eta1, double phi1, double eta2, double phi2) {
   return sqrt(deltaR2(eta1, phi1, eta2, phi2));
 }
 
 /// Get TVector2 for vector in the transverse plane
-TVector2 vectorSum(float pt1, float phi1, float pt2, float phi2) {
+TVector2 vectorSum(double pt1, double phi1, double pt2, double phi2) {
   TVector2 vec1;
   TVector2 vec2;
   vec1.SetMagPhi(pt1, phi1);
@@ -48,23 +59,23 @@ TVector2 vectorSum(float pt1, float phi1, float pt2, float phi2) {
 }
 
 /// Phi of a mother of two particles
-float vectorSumPhi(float pt1, float phi1, float pt2, float phi2) {
+double vectorSumPhi(double pt1, double phi1, double pt2, double phi2) {
   return vectorSum(pt1, phi1, pt2, phi2).Phi();
 }
 
 /// Pt of a mother of two particles
-float vectorSumPt(float pt1, float phi1, float pt2, float phi2) {
+double vectorSumPt(double pt1, double phi1, double pt2, double phi2) {
   return vectorSum(pt1, phi1, pt2, phi2).Mod();
 }
 
 /// Pt of a mother of three particles
-float vectorSum3Pt(float pt1, float phi1, float pt2, float phi2,float pt3, float phi3) {
+double vectorSum3Pt(double pt1, double phi1, double pt2, double phi2,double pt3, double phi3) {
   return sqrt( pow(pt1*cos(phi1) + pt2*cos(phi2) + pt3*cos(phi3),2) +
 	       pow(pt1*sin(phi1) + pt2*sin(phi2) + pt3*sin(phi3),2) );
 }
 
 /// Mass of a mother of two massive particles
-float vectorSumMass(float pt1, float eta1, float phi1, float mass1, float pt2, float eta2, float phi2, float mass2) {
+double vectorSumMass(double pt1, double eta1, double phi1, double mass1, double pt2, double eta2, double phi2, double mass2) {
   TLorentzVector vec1;
   TLorentzVector vec2;
   TLorentzVector vec3;
@@ -75,12 +86,12 @@ float vectorSumMass(float pt1, float eta1, float phi1, float mass1, float pt2, f
 }
 
 /// Mass of a mother of two massless particles
-float vectorSumMass(float pt1, float eta1, float phi1, float pt2, float eta2, float phi2) {
+double vectorSumMass(double pt1, double eta1, double phi1, double pt2, double eta2, double phi2) {
   return vectorSumMass(pt1,eta1,phi1,0.0,pt2,eta2,phi2,0.0);
 }
 
 /// Eta of a mother of two particles
-float vectorSumEta(float pt1, float eta1, float phi1, float pt2, float eta2, float phi2) {
+double vectorSumEta(double pt1, double eta1, double phi1, double pt2, double eta2, double phi2) {
   TLorentzVector vec1;
   TLorentzVector vec2;
   TLorentzVector vec3;
@@ -91,21 +102,21 @@ float vectorSumEta(float pt1, float eta1, float phi1, float pt2, float eta2, flo
 }
 
 /// Calculate uPerp given reconstructed recoil and boson phi
-float uPerp(float recoil, float recoilPhi, float zPhi) {
+double uPerp(double recoil, double recoilPhi, double zPhi) {
   TLorentzVector recoilvec;
   recoilvec.SetPtEtaPhiM(recoil,0,recoilPhi + TMath::Pi() - zPhi,0);
   return recoilvec.Py();
 }
 
 /// Calculate uPara given reconstructed recoil and boson phi
-float uPara(float recoil, float recoilPhi, float zPhi) {
+double uPara(double recoil, double recoilPhi, double zPhi) {
   TLorentzVector recoilvec;
   recoilvec.SetPtEtaPhiM(recoil,0,recoilPhi + TMath::Pi() - zPhi,0);
   return recoilvec.Px();
 }
 
 /// Calculate uPerp given reconstructed MET and boson pt and phi
-float uPerp(float met, float metPhi, float zPt, float zPhi) {
+double uPerp(double met, double metPhi, double zPt, double zPhi) {
   TLorentzVector metvec;
   metvec.SetPtEtaPhiM(vectorSumPt(met,metPhi,zPt,zPhi),0,vectorSumPhi(met,metPhi,zPt,zPhi)
                       + TMath::Pi() - zPhi,0);
@@ -113,7 +124,7 @@ float uPerp(float met, float metPhi, float zPt, float zPhi) {
 }
 
 /// Calculate uPara given reconstructed MET and boson pt and phi
-float uPara(float met, float metPhi, float zPt, float zPhi) {
+double uPara(double met, double metPhi, double zPt, double zPhi) {
   TLorentzVector metvec;
   metvec.SetPtEtaPhiM(vectorSumPt(met,metPhi,zPt,zPhi),0,vectorSumPhi(met,metPhi,zPt,zPhi)
                    + TMath::Pi() - zPhi,0);
@@ -121,7 +132,7 @@ float uPara(float met, float metPhi, float zPt, float zPhi) {
 }
 
 /// Calculates transverse mass given a lepton and MET
-float transverseMass(float lepPt, float lepPhi, float met,  float metPhi) {
+double transverseMass(double lepPt, double lepPhi, double met,  double metPhi) {
   double cosDPhi = cos(deltaPhi(lepPhi,metPhi));
   return sqrt(2*lepPt*met*(1-cosDPhi));
 }
