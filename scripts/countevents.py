73726b2f9e00ba647c4b37f5933564f6e29b5cf2
Daniel Abercrombie <dabercro@mit.edu>
2019-03-11 14:09:13 -0400

    Add a script to count number of events passing a cut from lots of files.

---
+++ scripts/countevents.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+import sys
+
+import ROOT
+
+if __name__ == '__main__':
+    for filename in sys.argv[2:]:
+        rootfile = ROOT.TFile(filename)
+        nevents = rootfile.events.GetEntries(sys.argv[1])
+        if nevents:
+            print '%s: %i' % (filename, nevents)
