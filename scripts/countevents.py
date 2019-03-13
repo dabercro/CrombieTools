fe572e47c3590c52055b8843d14e912f9a1a6c75
Daniel Abercrombie <dabercro@mit.edu>
2019-03-13 16:42:12 -0400

    Close file explicitly, just in case

--- scripts/countevents.py
+++ scripts/countevents.py
@@ -8,5 +8,6 @@ if __name__ == '__main__':
     for filename in sys.argv[2:]:
         rootfile = ROOT.TFile(filename)
         nevents = rootfile.events.GetEntries(sys.argv[1])
+        rootfile.Close()
         if nevents:
             print '%s: %i' % (filename, nevents)
