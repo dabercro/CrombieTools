34c01d0be0dd5350636b93c930ec913a28e9cc42
Daniel Abercrombie <dabercro@mit.edu>
2019-11-15 11:39:30 -0500

    Open remote files and dump branch types

--- scripts/printbranches.py
+++ scripts/printbranches.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+import os
 import sys
 
 sys.argv.append('-b')
@@ -8,5 +9,6 @@ import ROOT
 
 
 if __name__ == '__main__':
-    FILE = ROOT.TFile(sys.argv[1])
-    print '\n'.join([br.GetName() for br in FILE.events.GetListOfBranches()])
+    FILE = ROOT.TFile.Open(sys.argv[1])
+    print '\n'.join([br.Print() if os.environ.get('print') else br.GetName()
+                     for br in getattr(FILE, os.environ.get('tree', 'events')).GetListOfBranches()])
